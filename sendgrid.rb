a952974282200cc69d3d08e213505d48a62d23fd
felixprograms <felixprograms1205@gmail.com>
2020-09-27 01:29:59 +0000

    sendemail

--- sendgrid.rb
+++ sendgrid.rb
@@ -2,15 +2,17 @@
 # https://github.com/sendgrid/sendgrid-ruby
 require 'sendgrid-ruby'
 include SendGrid
+class SendEmail
+	def self.send(to_email, subject, content_value)
+		from = Email.new(email: ENV['EMAIL_ADDRESS'])
+		to = Email.new(email: to_email)
+		content = Content.new(type: 'text/plain', value: content_value)
+		mail = Mail.new(from, subject, to, content)
 
-from = Email.new(email: ENV['EMAIL_ADDRESS'])
-to = Email.new(email: ENV['EMAIL_ADDRESS'])
-subject = 'You created an account'
-content = Content.new(type: 'text/plain', value: 'Just to inform you that you created an account successfully.')
-mail = Mail.new(from, subject, to, content)
-
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail.to_json)
-puts response.status_code
-puts response.body
-puts response.headers
+		sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+		response = sg.client.mail._('send').post(request_body: mail.to_json)
+		puts response.status_code
+		puts response.body
+		puts response.headers
+	end
+end
