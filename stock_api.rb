14c03cb402e3da8e69ff3bf94b4fed010710726c
felixprograms <felixprograms1205@gmail.com>
2020-10-18 03:05:58 +0000

    redis

--- stock_api.rb
+++ stock_api.rb
@@ -1,11 +1,20 @@
 require 'faraday'
-
+require 'redis'
 
 class StockApi
 
     def self.get_stock_price(ticker_name)
-        response = Faraday.get "https://sandbox.iexapis.com/stable/stock/#{ticker_name}/price?token=Tsk_620fca762f4c427c98eead688e7df20f"
-        return Float(response.body)
+        redis = Redis.new
+				
+				if price = redis.get(ticker_name)
+					return Float(price)
+				else
+					p 'making api call'
+					response = Faraday.get("https://sandbox.iexapis.com/stable/stock/#{ticker_name}/price?token=Tsk_620fca762f4c427c98eead688e7df20f")
+					price = Float(response.body)
+					redis.setex(ticker_name, 60, price)
+        	return price
+				end
     end
 
 end
